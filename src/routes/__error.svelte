c59f6bc6dea8f3373d8e5869d79ea42501fbdb27
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-01-16 15:23:02 +0100

    Add Custom 404 Page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	import Logo from '/src/components/Logo.svelte';
+
+	export let title;
+</script>
+
+<h1
+	class="h-[calc(100vh-10rem)] flex items-center justify-center text-6xl font-os-light space-x-5 text-primary-content"
+>
+	<Logo width="20" height="20" />
+	<span>{title}</span>
+</h1>
